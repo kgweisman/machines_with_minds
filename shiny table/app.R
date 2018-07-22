e3736de5729ecf2ac05d2e4e292412c85abd669b
kgweisman <kweisman@stanford.edu>
2018-07-22 12:23:12 -0400

    undo malfunctioning checkboxes

--- shiny table/app.R
+++ shiny table/app.R
@@ -35,27 +35,101 @@ mean_tab <- read.csv(text = mean_url) %>%
 
 names(mean_tab) <- gsub("\\.", " ", names(mean_tab))
 
+# # VERSION 1:
+#
+# ui <- fluidPage(
+#   title = "Machines with Minds: Table",
+#   sidebarLayout(
+#     sidebarPanel(
+#       conditionalPanel(
+#         'input.dataset === "percentage_tab"',
+#         checkboxGroupInput("show_vars_percentage", "Select columns to dislay:",
+#                            names(percentage_tab), selected = names(percentage_tab))
+#       ),
+#       conditionalPanel(
+#         'input.dataset === "mean_tab"',
+#         checkboxGroupInput("show_vars_mean", "Select columns to dislay:",
+#                            names(mean_tab), selected = names(mean_tab))
+#       )
+#     ),
+#     mainPanel(
+#       tabsetPanel(
+#         id = 'dataset',
+#         tabPanel("percentage_tab", DT::dataTableOutput("mytable1")),
+#         tabPanel("mean_tab", DT::dataTableOutput("mytable2"))
+#       )
+#     )
+#   )
+# )
+# 
+# server <- function(input, output) {
+#   
+#   # choose columns to display
+#   percentage_tab2 = percentage_tab
+#   output$mytable1 <- DT::renderDataTable({
+#     DT::datatable(percentage_tab2[, input$show_vars_percentage, drop = FALSE])
+#   })
+#   
+#   # choose columns to display
+#   mean_tab2 = mean_tab
+#   output$mytable1 <- DT::renderDataTable({
+#     DT::datatable(mean_tab2[, input$show_vars_mean, drop = FALSE])
+#   })
+#   
+# }
+# 
+# shinyApp(ui, server)
+
+# # VERSION 2
+# ui <- fluidPage(
+#   title = "Machines with Minds: Tables",
+#   sidebarLayout(
+#     sidebarPanel(
+#       conditionalPanel(
+#         'input.dataset === "percentage_tab"',
+#         checkboxGroupInput("show_vars_percentage", "Select columns to display:",
+#                            names(percentage_tab), selected = names(percentage_tab))
+#       ),
+#       conditionalPanel(
+#         'input.dataset === "mean_tab"',
+#         checkboxGroupInput("show_vars_mean", "Select columns to display:",
+#                            names(mean_tab), selected = names(mean_tab))
+#       )
+#     ),
+#     mainPanel(
+#       tabsetPanel(
+#         id = 'dataset',
+#         tabPanel("percentages", DT::dataTableOutput("mytable1")),
+#         tabPanel("means", DT::dataTableOutput("mytable2"))
+#       )
+#     )
+#   )
+# )
+# 
+# server <- function(input, output) {
+#   
+#   # choose columns to display
+#   percentage_tab2 = percentage_tab
+#   output$mytable1 <- DT::renderDataTable({
+#     DT::datatable(percentage_tab2[, input$show_vars_percentage, drop = FALSE])
+#   })
+#   
+#   # choose columns to display
+#   mean_tab2 = mean_tab
+#   output$mytable2 <- DT::renderDataTable({
+#     DT::datatable(mean_tab2[, input$show_vars_mean, drop = FALSE])
+#   })
+# 
+# }
+
+# VERSION 3
 ui <- fluidPage(
-  title = "Examples of DataTables",
-  sidebarLayout(
-    sidebarPanel(
-      conditionalPanel(
-        'input.dataset === "percentage_tab"',
-        checkboxGroupInput("show_vars_percentage", "Select columns to dislay:",
-                           names(percentage_tab), selected = names(percentage_tab))
-      ),
-      conditionalPanel(
-        'input.dataset === "mean_tab"',
-        checkboxGroupInput("show_vars_mean", "Select columns to dislay:",
-                           names(mean_tab), selected = names(mean_tab))
-      )
-    ),
+  title = "Machines with Minds: Tables",
     mainPanel(
       tabsetPanel(
         id = 'dataset',
-        tabPanel("percentage_tab", DT::dataTableOutput("mytable1")),
-        tabPanel("mean_tab", DT::dataTableOutput("mytable2"))
-      )
+        tabPanel("percentages", DT::dataTableOutput("mytable1")),
+        tabPanel("means", DT::dataTableOutput("mytable2"))
     )
   )
 )
@@ -65,13 +139,15 @@ server <- function(input, output) {
   # choose columns to display
   percentage_tab2 = percentage_tab
   output$mytable1 <- DT::renderDataTable({
-    DT::datatable(percentage_tab2[, input$show_vars_percentage, drop = FALSE])
+    # DT::datatable(percentage_tab2[, input$show_vars_percentage, drop = FALSE])
+    DT::datatable(percentage_tab2)
   })
   
   # choose columns to display
   mean_tab2 = mean_tab
-  output$mytable1 <- DT::renderDataTable({
-    DT::datatable(mean_tab2[, input$show_vars_mean, drop = FALSE])
+  output$mytable2 <- DT::renderDataTable({
+    # DT::datatable(mean_tab2[, input$show_vars_mean, drop = FALSE])
+    DT::datatable(mean_tab2)
   })
   
 }
